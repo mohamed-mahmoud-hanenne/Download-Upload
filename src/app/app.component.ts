b5afc17005e6c8f123870977fc86b83aa898f794
mohamed mahmoud hanenne <medhanenne38@gmail.com>
2024-04-25 09:57:50 +0000

    first-commit-upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Download-Upload';
+  inputData: string = '';
+  apiFile = 'https://api.escuelajs.co/api/v1/files/';
+  uploadFileName: string[] = [];
+
+  constructor(private http:HttpClient){}
+
+  uploadImage(event : any){
+    
+    const file =  event.currentTarget.files[0];
+    if(file.type === 'image/png' && file.size === 2000000){
+      const formObject = new FormData();
+      formObject.append('file', file)
+      console.log(formObject)
+    }
+  }
 }
